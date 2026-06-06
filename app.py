2a3c5d4323504c6f0a8a58788bac83f10cf1e33c
mariadwia <mariadwii792@gmail.com>
2026-06-06 15:11:43 +0700

    Memperbaiki error CORPS untuk domain thesimple.my.id

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sklearn.neighbors import NearestNeighbors
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://thesimple.my.id"])
 
 # Load data
 df = None
